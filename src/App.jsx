643f5d9f4a2c48fdc3606f27b2cc4e279804fb60
edualt <eduardosebastianhdz503@gmail.com>
2023-04-18 14:51:00 -0600

    feat(components): add admin side bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Button from './components/Button/Button'
+import Input from './components/Input/Input'
+import NavBar from './containers/NavBar/NavBar'
+import Card from './containers/Card/Card'
+import AdminSideBar from './components/AdminSideBar/AdminSideBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+    <NavBar />
+    {/* <Button>Hola</Button>
+    <br />
+    Hola hola
+    <br />
+    <label>Email:</label>
+    <Input placeholder={"Email"}/>
+    <Input placeholder={"Roles"} type="select"/>
+    <Card /> */}
+    <AdminSideBar />
+    </>
   )
 }
 
