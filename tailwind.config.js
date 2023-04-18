daa83cb095e521a63b0f2caf11ef02ce4ce4ae94
edualt <eduardosebastianhdz503@gmail.com>
2023-04-18 14:50:03 -0600

    feat(styles): add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'principal-color': '#38A3A5',
+        'hover-color': '#22577A',
+        'light-color': '#80ED99',
+        'light-white': '#C7F9CC',
+        'intermidiate': '#57CC99',
+        'gray-custom': '#D9D9D9',
+      },
+      fontFamily: {
+        'lato': ['Lato', 'sans-serif'],
+      }
+
+    },
   },
   plugins: [],
 }
