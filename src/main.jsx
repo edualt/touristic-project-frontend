4b1d51259e8423bc703423b54bb88986fe3e07cf
Darinel Monjaras Ruiz <darinelmr12345@gmail.com>
2023-04-19 08:37:45 -0600

    register created

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import Login from './pages/Login'
+import Register from './pages/Register'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Login />
+    <Register />
   </React.StrictMode>,
 )
