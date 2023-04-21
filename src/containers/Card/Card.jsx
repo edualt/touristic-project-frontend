d0206d3f36a87845cded9d782a20e64726f0eab3
edualt <eduardosebastianhdz503@gmail.com>
2023-04-21 13:42:04 -0600

    fix(card): change size

--- src/containers/Card/Card.jsx
+++ src/containers/Card/Card.jsx
@@ -1,13 +1,16 @@
-function Card({ product }) {
+import Image from "../../assets/img/image-login.jpg";
+
+function Card({ site }) {
   return (
-    <div className='flex flex-col w-1/6 shadow-md'>
-        <div className='bg-gray-200 w-fit'>
-          <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Ca%C3%B1%C3%B3n_del_Sumidero%2C_Octubre_2020_-_27.jpg/1024px-Ca%C3%B1%C3%B3n_del_Sumidero%2C_Octubre_2020_-_27.jpg"} className='w-60 h-60 object-cover' />
-        </div>
-      <div className='flex flex-col w-full h-full bg-white px-6 py-5 gap-10'>
-          <p className='font-normal text-lg'>Nombre del lugar</p>
-          <p className='font-semibold'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, nec aliquet nisl nisl sit amet nisl. </p>
+    <div className='flex flex-col w-72 shadow-md'>
+      <div className='w-full h-48'>
+        <img className='w-full h-full object-cover' src={Image} alt="" />
       </div>
+
+      <div className='flex flex-col p-4'>
+        <h2 className='text-2xl font-bold'>Name</h2>
+        <p className='text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+        </div>
     </div>
   );
 }
